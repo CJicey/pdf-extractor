4fb8e1595aef5e87121ddab60b66b30f0659caba
Caleb Johnson <cjohn734@students.kennesaw.edu>
2025-07-30 08:45:33 -0400

    Delete Book of Knowledge directory

--- Book of Knowledge/Fields/alldata.py
+++
@@ -1,8 +0,0 @@
-class AllDataExtractor:
-    def __init__(self, raw_text=""):
-        self.raw_text = raw_text
-
-    def to_dict(self):
-        return {
-            "raw_text": self.raw_text
-        }
